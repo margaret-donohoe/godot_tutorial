a421690903b391b5e7a929983863e794d3083249
Margaret Donohoe <margaretrdonohoe@gmail.com>
2025-03-03 11:03:51 -0500

    3.3.2025

--- scripts/Character_Movement.gd
+++ scripts/Character_Movement.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
-@export var speed : float = 10.0
+@export var speed : float = 200
+
+var canInteract : bool = false;
+signal interact(obj_interact)
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +15,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("up"):
+		velocity.y = -speed
+	elif Input.is_action_pressed("down"):
+		velocity.y = speed
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+	
+	else:
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
+	
+	if canInteract and Input.is_action_just_pressed("Interact"):
+		interact.emit()
--- scripts/demoStart.gd
+++ scripts/demoStart.gd
@@ -6,9 +6,27 @@ var main_char = load("res://scenes/Character.tscn")
 func _ready():
 	var m_c = main_char.instantiate()
 	add_child(m_c)
+	m_c.interact.connect(_on_interact.bind($ZONE))
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_zone_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		body.canInteract = true
+		
+	pass # Replace with function body.
+
+func _on_zone_exited(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		body.canInteract = false
+	pass # Replace with function body.
+	
+func _on_interact(node):
+	if node.visible == true:
+		node.visible = false
+	elif node.visible == false:
+		node.visible = true
